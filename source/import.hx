fe147dce2280fc59cdd2d93dfa7a3067669fa42e
affsvoyo <githubunoficial@gmail.com>
2026-03-14 20:59:13 -0300

    Update import.hx

--- source/import.hx
+++ source/import.hx
@@ -1,1 +1,40 @@
+#if !macro
 import Paths;
+
+#if sys
+import sys.*;
+import sys.io.*;
+#elseif js
+import js.html.*;
+#end
+// Lua
+#if LUA_ALLOWED
+import llua.Lua;
+import llua.LuaL;
+import llua.State;
+import llua.Convert;
+#end
+
+//Flixel
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.FlxCamera;
+import flixel.util.FlxColor;
+import flixel.text.FlxText;
+import flixel.util.FlxTimer;
+import flixel.math.FlxMath;
+import flixel.math.FlxPoint;
+import flixel.tweens.FlxEase;
+#if !NEW_PSYCH063
+import flixel.system.FlxSound;
+#else
+import flixel.sound.FlxSound;
+#end
+import flixel.util.FlxDestroyUtil;
+import flixel.tweens.FlxTween;
+import flixel.group.FlxSpriteGroup;
+import flixel.group.FlxGroup.FlxTypedGroup;
+import flixel.graphics.frames.FlxAtlasFrames;
+#end
+
+using StringTools;
